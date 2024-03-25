8254c1f1aceedd34fd5912c249c03516d7668674
kadirkaang <kadirkaang52@gmail.com>
2024-03-25 17:00:07 +0300

    feat: Changes welcome message
    BREAKING CHANGE:
    Message changed from "Hello" to "Welcome"

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.get("/", function (req, res) {
+	console.log("Home page visited")
     res.sendFile(__dirname + '/views/index.html');
 });
 
